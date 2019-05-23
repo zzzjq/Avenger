932731340f47b8e14cb277fb20a7d0d933a9fbbc
zhangjingqi <zhangjingqiyy@sina.com>
2019-05-23 14:47:01 +0800

    avenger assemble

---
+++ Avenger-api/src/main/java/avenger/api/enums/AvengerEnum.java
@@ -0,0 +1,10 @@
+package avenger.api.enums;
+/**
+ * 
+ * @author zhangjingqi
+ * @date 2019年5月23日下午2:28:59
+ *
+ **/
+public enum AvengerEnum {
+
+}
---
+++ Avenger-api/src/main/java/avenger/api/facade/AvengerFacade.java
@@ -0,0 +1,13 @@
+package avenger.api.facade;
+
+/**
+ * AvengerFacade
+ * 
+ * @author zhangjingqi
+ * @date 2019年5月23日下午2:26:46
+ *
+ **/
+public interface AvengerFacade {
+
+	void ping();
+}
