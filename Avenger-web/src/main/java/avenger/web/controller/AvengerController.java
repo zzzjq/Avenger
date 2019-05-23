3673ea62fb2a693724c24cc1edcc368c8d859e7c
zhangjingqi <zhangjingqiyy@sina.com>
2019-05-23 17:45:05 +0800

    avenger

--- Avenger-web/src/main/java/avenger/web/controller/AvengerController.java
+++ Avenger-web/src/main/java/avenger/web/controller/AvengerController.java
@@ -1,10 +1,14 @@
 package avenger.web.controller;
+
+import org.springframework.stereotype.Controller;
+
 /**
  * 
  * @author zhangjingqi
  * @date 2019年5月23日下午5:38:47
  *
  **/
+@Controller
 public class AvengerController {
 
 }
